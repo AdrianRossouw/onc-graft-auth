af9d0cf1b95223dc2c12c6f1f7d59f5a57c16e04
Adrian Rossouw <adrian@daemon.co.za>
2013-07-26 15:15:16 +0200

    last changes.

--- middleware/Auth.Dummy.graft.js
+++ middleware/Auth.Dummy.graft.js
@@ -1,7 +1,7 @@
 var Strategy = require('passport-dummy').Strategy;
 
-var Passport = Graft.module('Middleware.Auth');
 
 this.addInitializer(function(opts) {
-    this.strategy = Passport.request('createStrategy', 'dummy', Strategy, {});
+    this.strategy = Graft.Middleware.Auth.request('createStrategy', 'dummy', Strategy, {});
+
 });
