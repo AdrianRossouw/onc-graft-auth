615d605f6ad579dab9df642f990964ed5dc2e5ea
Adrian Rossouw <adrian@daemon.co.za>
2013-10-19 18:21:14 -0700

    auth actually does a fetch niow.

--- auth/local.js
+++ auth/local.js
@@ -8,24 +8,23 @@ this.addInitializer(function(opts) {
 });
 
 Graft.Auth.commands.setHandler('verify:local', function(username, password, done) {
-  var user = new Graft.Auth.Model();
-  if(user.get('username') == username) {
-    if(user.get('password') == user.hash(password)) {
-      done(null, { id: user.get('username') });
-    } else {
-      done('Incorrect password!', user);
+    var user = new Graft.Auth.Model({id: username});
+    user.fetch().then(doneFn, failFn);
+
+    function failFn(model, res, options) {
+        done('Username not found!');
     }
-  } else {
-    done('Username not found!', user);
-  }
-//  user.fetch({
-//    error: function(model, res, opts) {
-//      done(res, user);
-//    },
-//    success: function() {
-//      console.log('Success');
-//      done(null, {id: 'local'});
-//    }
-//  })
-  //console.log(user.hash(password));
+
+    function doneFn(model, res, opts) {
+        if(user.get('id') == username) {
+            if(user.get('password') == user.hash(password)) {
+                done(null, { id: user.get('id') });
+            } else {
+                done('Incorrect password!');
+            }
+        } else {
+            done('Unknown error!');
+        }
+
+    }
 }, this);
